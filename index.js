6626e8dd63fb75adf2b5fee34119d52028c16ef8
raaj <raaj@gmail.com>
2022-01-04 23:08:30 +0530

    key change

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const accountSid = 'ACc7d6e0aa827c5b77a0986fdb40651831'; 
-const authToken = '4021740eec2538f14ec5a2992f5bdc39'; 
-const client = require('twilio')(accountSid, authToken); 
-
-// console.log("hello world")
-// const server=http.createServer((req,res)=>{
-//   if(req.url==='/'){
-//           console.log("Get Method Called");
-//           client.messages
-//           .create({
-//               body: `Here's that picture of an owl you requested.`,
-//             from: 'whatsapp:+14155238886',
-//             to: 'whatsapp:+918667849150'
-//           }).then(message => console.log(message.sid));
-
-//   }
-//   if (req.url==='/api/courses'){
-
-//   }
-// });
-// server.listen(4000);
 
 
 const express=require('express');
@@ -33,14 +12,18 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/sendmessage',(req,res)=>{
-  console.log("Get Method Called");
-          client.messages
-          .create({
-              body:req.body.message,
+  console.log("service started",req.body.message)
+const accountSid = 'AC838ca38a5d59848e369909586b255e06'; 
+const authToken = '18d03a26f714cceebfb50e872c109d1a'; 
+const client = require('twilio')(accountSid, authToken); 
+  client.messages.create({
+            body:req.body.message,
             from: 'whatsapp:+14155238886',
             to: 'whatsapp:+918667849150'
           }).then(message => res.send(message.sid));
 
 })
 
-app.listen(process.env.PORT||4000);
+
+const port=process.env.PORT||4000;
+app.listen(port,()=>console.log(`Listening 4000...`))
