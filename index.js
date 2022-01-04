d6b66f3a2c38e3f5a425e6f40261ce5cb92dee58
raaj <raaj@gmail.com>
2022-01-04 23:59:33 +0530

    working Msg Sending Api

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 
-
+const accountSid = 'AC838ca38a5d59848e369909586b255e06'; 
+const authToken = '088691a64cf5d31363bb253ad2548867'; 
+const client = require('twilio')(accountSid, authToken); 
 const express=require('express');
 const app=express();
-const accountSid = 'AC838ca38a5d59848e369909586b255e06'; 
-const authToken = '18d03a26f714cceebfb50e872c109d1a'; 
-const client = require('twilio')(accountSid, authToken); 
-app.use(express.urlencoded({ extended: true }));
 
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.get('/',(req,res)=>{
   res.send('hello world');
 })
 
-app.post('/testsite',(req,res)=>{
-  res.send("Respose")
-})
-
 app.post('/sendmessage',(req,res)=>{
-  console.log("service started",req.body.message)
-
-  client.messages.create({
-            body:req.body.message,
-            from: 'whatsapp:+14155238886',
-            to: 'whatsapp:+918667849150'
-          }).then(message => res.send(message.sid));
+  console.log("service started",req)
+  client.messages 
+      .create({ 
+         body: String(req.body.message), 
+         from: 'whatsapp:+14155238886',       
+         to: 'whatsapp:+918667849150' 
+       }) 
+      .then(messages => res.send(messages.sid)) 
+      .done();
 
 })
 
