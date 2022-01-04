5f621cee3e1da0929cfccef28e35064d44e9b6a7
raaj <raaj@gmail.com>
2022-01-04 23:18:07 +0530

    add anothersite

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const express=require('express');
 const app=express();
-
+const accountSid = 'AC838ca38a5d59848e369909586b255e06'; 
+const authToken = '18d03a26f714cceebfb50e872c109d1a'; 
+const client = require('twilio')(accountSid, authToken); 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
@@ -11,11 +13,13 @@ app.get('/',(req,res)=>{
   res.send('hello world');
 })
 
+app.post('/testsite',(req,res)=>{
+  res.send("Respose")
+})
+
 app.post('/sendmessage',(req,res)=>{
   console.log("service started",req.body.message)
-const accountSid = 'AC838ca38a5d59848e369909586b255e06'; 
-const authToken = '18d03a26f714cceebfb50e872c109d1a'; 
-const client = require('twilio')(accountSid, authToken); 
+
   client.messages.create({
             body:req.body.message,
             from: 'whatsapp:+14155238886',
