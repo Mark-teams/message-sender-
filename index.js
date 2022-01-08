255b016c10171cd526d80ed1fda0b8d8ea644b10
raaj <raaj@gmail.com>
2022-01-08 11:52:44 +0530

    key

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const { response } = require('express');
 const express=require('express');
-const bodyParser=require('body-parser');
 const cors = require('cors');
 const app=express();
 const dotenv=require('dotenv')
